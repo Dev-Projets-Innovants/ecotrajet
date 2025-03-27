fd66e8cc617cbcc3e31dd039ac4ba7bb7d753588
Forestin WAMBA <121468179+Wambaforestin@users.noreply.github.com>
2025-03-27 11:00:13 +0000

    mise à jour image here

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -12,7 +12,7 @@ const Hero = () => {
         <div 
           className="absolute inset-0 bg-cover bg-center"
           style={{ 
-            backgroundImage: 'url("https://images.unsplash.com/photo-1471623432079-b009d30b6729?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80")',
+            backgroundImage: 'url("https://www.apur.org/sites/default/files/images/publication/a-la-une/mobilites_emergentes_935_550.jpg")',
             backgroundPosition: 'center'
           }}
         ></div>
